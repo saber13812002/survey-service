dba9606b41c75cf5791b2385d5095771f9689945
saber13812002 <saber.tabatabaee@gmail.com>
2021-06-08 16:43:43 +0430

    rename all route in singular form into plural

--- routes/api.php
+++ routes/api.php
@@ -26,11 +26,11 @@ Route::group([
         Route::middleware(['auth:access_token'])->group(function () {
 
             //client-app
-            Route::get('/client/apps', [Controllers\v1\ClientAppController::class, 'index'])->name('client.app.index');
-            Route::get('/client/apps/{id}', [Controllers\v1\ClientAppController::class, 'show'])->name('client.app.show');
-            Route::post('/client/apps', [Controllers\v1\ClientAppController::class, 'store'])->name('client.app.store');
-            Route::put('/client/apps/{id}', [Controllers\v1\ClientAppController::class, 'update'])->name('client.app.update');
-            Route::delete('/client/apps/{id}', [Controllers\v1\ClientAppController::class, 'destroy'])->name('client.app.destroy');
+            Route::get('/apps', [Controllers\v1\ClientAppController::class, 'index'])->name('client.app.index');
+            Route::get('/apps/{id}', [Controllers\v1\ClientAppController::class, 'show'])->name('client.app.show');
+            Route::post('/apps', [Controllers\v1\ClientAppController::class, 'store'])->name('client.app.store');
+            Route::put('/apps/{id}', [Controllers\v1\ClientAppController::class, 'update'])->name('client.app.update');
+            Route::delete('/apps/{id}', [Controllers\v1\ClientAppController::class, 'destroy'])->name('client.app.destroy');
 
             //category
             Route::get('/categories', [Controllers\v1\CategoryController::class, 'index'])->name('category.index');
@@ -54,7 +54,10 @@ Route::group([
             Route::delete('/tags/{id}', [Controllers\v1\TagController::class, 'destroy'])->name('tag.destroy');
 
             // package Type
-            Route::get('/package-types', [Controllers\v1\PackageTypeController::class, 'index'])->name('package.type.index');
+            Route::get('/packages/types', [Controllers\v1\PackageTypeController::class, 'index'])->name('package.type.index');
+
+            // answer Type
+            Route::get('/answers/types', [Controllers\v1\AnswerTypeController::class, 'index'])->name('answer.type.index');
 
             // package
             Route::get('/packages', [Controllers\v1\PackageController::class, 'index'])->name('package.index');
@@ -64,43 +67,40 @@ Route::group([
             Route::delete('/packages/{id}', [Controllers\v1\PackageController::class, 'destroy'])->name('package.destroy');
 
             // category to package
-            Route::put('/package/{package_id}/categories', [Controllers\v1\CategorizableController::class, 'connect'])->name('connect.category.to.package');
-            Route::delete('/package/{package_id}/categories', [Controllers\v1\CategorizableController::class, 'disconnect'])->name('disconnect.category.from.package');
+            Route::put('/packages/{packageId}/categories', [Controllers\v1\CategorizableController::class, 'connect'])->name('connect.category.to.package');
+            Route::delete('/packages/{packageId}/categories', [Controllers\v1\CategorizableController::class, 'disconnect'])->name('disconnect.category.from.package');
 
             // campaign to package
-            Route::put('package/{package_id}/campaigns', [Controllers\v1\CampanileController::class, 'connect'])->name('connect.campaign.to.package');
-            Route::delete('package/{package_id}/campaigns', [Controllers\v1\CampanileController::class, 'disconnect'])->name('disconnect.campaign.from.package');
+            Route::put('packages/{packageId}/campaigns', [Controllers\v1\CampanileController::class, 'connect'])->name('connect.campaign.to.package');
+            Route::delete('packages/{packageId}/campaigns', [Controllers\v1\CampanileController::class, 'disconnect'])->name('disconnect.campaign.from.package');
 
             // tag to package
-            Route::put('package/{package_id}/tags', [Controllers\v1\TaggableController::class, 'connect'])->name('connect.tag.to.package');
-            Route::delete('package/{package_id}/tags', [Controllers\v1\TaggableController::class, 'disconnect'])->name('disconnect.tag.from.package');
-
-            // answer Type
-            Route::get('/answer-types', [Controllers\v1\AnswerTypeController::class, 'index'])->name('answer.type.index');
+            Route::put('packages/{packageId}/tags', [Controllers\v1\TaggableController::class, 'connect'])->name('connect.tag.to.package');
+            Route::delete('packages/{packageId}/tags', [Controllers\v1\TaggableController::class, 'disconnect'])->name('disconnect.tag.from.package');
 
             // question
-            Route::get('/questions', [Controllers\v1\PackageQuestionController::class, 'index'])->name('package.question.index');
+            Route::get('/questions/packages/{id}', [Controllers\v1\PackageQuestionController::class, 'index'])->name('package.question.index');
             Route::get('/questions/{id}', [Controllers\v1\PackageQuestionController::class, 'show'])->name('package.question.show');
             Route::post('/questions', [Controllers\v1\PackageQuestionController::class, 'store'])->name('package.question.store');
             Route::put('/questions/{id}', [Controllers\v1\PackageQuestionController::class, 'update'])->name('package.question.update');
             Route::delete('/questions/{id}', [Controllers\v1\PackageQuestionController::class, 'destroy'])->name('package.question.destroy');
 
             // question choice
-            Route::get('/question/{id}/choices', [Controllers\v1\PackageQuestionChoiceController::class, 'index'])->name('package.question.choice.index');
-            Route::get('/question/choices/{id}', [Controllers\v1\PackageQuestionChoiceController::class, 'show'])->name('package.question.choice.show');
-            Route::post('/question/{id}/choices', [Controllers\v1\PackageQuestionChoiceController::class, 'store'])->name('package.question.choice.store');
-            Route::post('/question/choices', [Controllers\v1\PackageQuestionChoiceController::class, 'storeBulk'])->name('package.question.choice.store.bulk');
-            Route::put('/question/choices/{id}', [Controllers\v1\PackageQuestionChoiceController::class, 'update'])->name('package.question.choice.update');
-            Route::delete('/question/choices/{id}', [Controllers\v1\PackageQuestionChoiceController::class, 'destroy'])->name('package.question.choice.destroy');
+            Route::get('/questions/{id}/choices', [Controllers\v1\PackageQuestionChoiceController::class, 'index'])->name('package.question.choice.index');
+            Route::get('/questions/choices/{id}', [Controllers\v1\PackageQuestionChoiceController::class, 'show'])->name('package.question.choice.show');
+            Route::post('/questions/{id}/choices', [Controllers\v1\PackageQuestionChoiceController::class, 'store'])->name('package.question.choice.store');
+            Route::post('/questions/choices', [Controllers\v1\PackageQuestionChoiceController::class, 'storeBulk'])->name('package.question.choice.store.bulk');
+            Route::put('/questions/choices/{id}', [Controllers\v1\PackageQuestionChoiceController::class, 'update'])->name('package.question.choice.update');
+            Route::delete('/questions/choices/{id}', [Controllers\v1\PackageQuestionChoiceController::class, 'destroy'])->name('package.question.choice.destroy');
 
             // package answer
-            Route::get('/package/{id}/answers', [Controllers\v1\PackageAnswerController::class, 'index'])->name('package.question.choice.index');
-            Route::get('/question/answers/{id}', [Controllers\v1\PackageAnswerController::class, 'show'])->name('package.question.choice.show');
-            Route::get('/question/{id}/answers', [Controllers\v1\PackageAnswerController::class, 'showByQuestionId'])->name('package.question.choice.show.by.question.id');
-            Route::post('/question/{id}/answers', [Controllers\v1\PackageAnswerController::class, 'store'])->name('package.question.choice.store');
-//            Route::post('/question/answers', [Controllers\v1\PackageAnswerController::class, 'storeBulk'])->name('package.question.choice.store.bulk');
-            Route::put('/question/{question_id}/answers', [Controllers\v1\PackageAnswerController::class, 'update'])->name('package.question.choice.update');
-            Route::delete('/question/answers/{id}', [Controllers\v1\PackageAnswerController::class, 'destroy'])->name('package.question.choice.destroy');
+            Route::get('/packages/{id}/answers', [Controllers\v1\PackageAnswerController::class, 'index'])->name('package.question.choice.index');
+            Route::get('/questions/answers/{id}', [Controllers\v1\PackageAnswerController::class, 'show'])->name('package.question.choice.show');
+            Route::get('/questions/{id}/answers', [Controllers\v1\PackageAnswerController::class, 'getByQuestionId'])->name('package.question.choice.show.by.question.id');
+            Route::post('/questions/{id}/answers', [Controllers\v1\PackageAnswerController::class, 'store'])->name('package.question.choice.store');
+//            Route::post('/questions/answers', [Controllers\v1\PackageAnswerController::class, 'storeBulk'])->name('package.question.choice.store.bulk');
+            Route::put('/questions/{id}/answers', [Controllers\v1\PackageAnswerController::class, 'update'])->name('package.question.choice.update');
+            Route::delete('/questions/answers/{id}', [Controllers\v1\PackageAnswerController::class, 'destroy'])->name('package.question.choice.destroy');
 
             //test
             Route::get('/test', [Controllers\TestController::class, 'test']);
