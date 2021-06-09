309628dd94ef3960824743522ea383c41e350515
saber13812002 <saber.tabatabaee@gmail.com>
2021-06-09 16:58:49 +0430

    Document campaigns apis then test as well

--- app/Http/Controllers/v1/CampaignController.php
+++ app/Http/Controllers/v1/CampaignController.php
@@ -13,7 +13,35 @@ use Illuminate\Http\Request;
 class CampaignController extends Controller
 {
     /**
-     * Display a listing of the resource.
+    * @OA\Get(
+    *  path="/api/v1/campaigns",
+    *  operationId="getListOfCampaigns",
+    *  summary="get list of all campaigns",
+    *  tags={"Campaigns"},
+    *
+    *  @OA\Parameter(
+    *       name="access_token",
+    *       required=true,
+    *       in="header",
+    *       example="4fVB9SZidiBAADD2333nLZxxbWk92UcPQkwM8k",
+    *       @OA\Schema(
+    *           type="string"
+    *       )
+    *   ),
+    *
+    *   @OA\Response(
+    *      response=200,
+    *       description="Success",
+    *      @OA\MediaType(
+    *           mediaType="application/json",
+    *      )
+    *   ),
+    *   @OA\Response(
+    *      response=404,
+    *      description="not found"
+    *   ),
+    *)
+     * Display a listing of the campaigns resource.
      *
      * @return CampaignResourceCollection
      */
@@ -24,7 +52,42 @@ class CampaignController extends Controller
     }
 
     /**
-     * Store a newly created resource in storage.
+    * @OA\Post(
+    *  path="/api/v1/campaigns",
+    *  operationId="postANewCampaign",
+    *  summary="define a new campaign",
+    *  tags={"Campaigns"},
+    *
+    *  @OA\Parameter(
+    *       name="access_token",
+    *       required=true,
+    *       in="header",
+    *       example="4fVB9SZidiBAADD2333nLZxxbWk92UcPQkwM8k",
+    *       @OA\Schema(
+    *           type="string"
+    *       )
+    *   ),
+    *
+    *   @OA\RequestBody(
+    *       required=true,
+    *       @OA\JsonContent(ref="#/components/schemas/CampaignStoreRequest")
+    *   ),
+    *
+    *   @OA\Response(
+    *      response=200,
+    *       description="Success",
+    *      @OA\MediaType(
+    *           mediaType="application/json",
+    *      )
+    *   ),
+    *
+    *   @OA\Response(
+    *      response=404,
+    *      description="not found"
+    *   ),
+    *)
+    *
+     * Store a newly created campaign resource in storage.
      *
      * @param  \Illuminate\Http\Request  $request
      * @return CampaignResource
@@ -36,7 +99,48 @@ class CampaignController extends Controller
     }
 
     /**
-     * Display the specified resource.
+         * @OA\Get(
+         *  path="/api/v1/campaigns/{campaignId}",
+         *  operationId="getCampaignItemById",
+         *  summary="get item by id",
+         *  tags={"Campaigns"},
+         *
+         *  @OA\Parameter(
+         *       name="access_token",
+         *       required=true,
+         *       in="header",
+         *       example="4fVB9SZidiBAADD2333nLZxxbWk92UcPQkwM8k",
+         *       @OA\Schema(
+         *           type="string"
+         *       )
+         *   ),
+         *
+         *  @OA\Parameter(
+         *       description="ID of campaign",
+         *       name="campaignId",
+         *       required=true,
+         *       in="path",
+         *       example="1",
+         *       @OA\Schema(
+         *           type="integer",
+         *           format="int64"
+         *       )
+         *   ),
+         *
+         *   @OA\Response(
+         *      response=200,
+         *       description="Success",
+         *      @OA\MediaType(
+         *           mediaType="application/json",
+         *      )
+         *   ),
+         *   @OA\Response(
+         *      response=404,
+         *      description="not found"
+         *   ),
+         *)
+         *
+     * Display the specified campaign resource.
      *
      * @param int $id
      * @return CampaignResource
@@ -49,13 +153,61 @@ class CampaignController extends Controller
     }
 
     /**
-     * Update the specified resource in storage.
-     *
-     * @param \Illuminate\Http\Request $request
-     * @param int $id
-     * @return CampaignResource
-     * @throws \Illuminate\Contracts\Container\BindingResolutionException
-     */
+    * @OA\Put(
+    *  path="/api/v1/campaigns/{campaignId}",
+    *  operationId="updateCampaignItemById",
+    *  summary="update campaign item by id",
+    *  tags={"Campaigns"},
+    *
+    *  @OA\Parameter(
+    *       name="access_token",
+    *       required=true,
+    *       in="header",
+    *       example="4fVB9SZidiBAADD2333nLZxxbWk92UcPQkwM8k",
+    *       @OA\Schema(
+    *           type="string"
+    *       )
+    *   ),
+    *
+    *  @OA\Parameter(
+    *       description="ID of campaign",
+    *       name="campaignId",
+    *       required=true,
+    *       in="path",
+    *       example="1",
+    *       @OA\Schema(
+    *           type="integer",
+    *           format="int64"
+    *       )
+    *   ),
+    *
+    *   @OA\RequestBody(
+    *       required=true,
+    *       @OA\JsonContent(ref="#/components/schemas/CampaignStoreRequest")
+    *   ),
+    *
+    *   @OA\Response(
+    *      response=200,
+    *       description="Success",
+    *      @OA\MediaType(
+    *           mediaType="application/json",
+    *      )
+    *   ),
+    *
+    *   @OA\Response(
+    *      response=404,
+    *      description="not found"
+    *   ),
+    *)
+    *
+    *
+    * Update the specified campaign resource in storage.
+    *
+    * @param \Illuminate\Http\Request $request
+    * @param int $id
+    * @return CampaignResource
+    * @throws \Illuminate\Contracts\Container\BindingResolutionException
+    */
     public function update(Request $request, int $id): CampaignResource
     {
         return new CampaignResource(["data" => app()->make(CampaignRepositoryInterface::class)
@@ -63,12 +215,53 @@ class CampaignController extends Controller
     }
 
     /**
-     * Remove the specified resource from storage.
-     *
-     * @param int $id
-     * @return CampaignResource
-     * @throws \Illuminate\Contracts\Container\BindingResolutionException
-     */
+    * @OA\Delete(
+    *  path="/api/v1/campaigns/{campaignId}",
+    *  operationId="removeAnItemById",
+    *  summary="remove item by id",
+    *  tags={"Campaigns"},
+    *
+    *  @OA\Parameter(
+    *       name="access_token",
+    *       required=true,
+    *       in="header",
+    *       example="4fVB9SZidiBAADD2333nLZxxbWk92UcPQkwM8k",
+    *       @OA\Schema(
+    *           type="string"
+    *       )
+    *   ),
+    *
+    *  @OA\Parameter(
+    *       description="ID of campaign",
+    *       name="campaignId",
+    *       required=true,
+    *       in="path",
+    *       example="1",
+    *       @OA\Schema(
+    *           type="integer",
+    *           format="int64"
+    *       )
+    *   ),
+    *
+    *   @OA\Response(
+    *      response=200,
+    *       description="Success",
+    *      @OA\MediaType(
+    *           mediaType="application/json",
+    *      )
+    *   ),
+    *   @OA\Response(
+    *      response=404,
+    *      description="not found"
+    *   ),
+    *)
+    *
+    * Remove the specified campaign resource from storage.
+    *
+    * @param int $id
+    * @return CampaignResource
+    * @throws \Illuminate\Contracts\Container\BindingResolutionException
+    */
     public function destroy(int $id): CampaignResource
     {
         return new CampaignResource(["data" => app()->make(CampaignRepositoryInterface::class)
