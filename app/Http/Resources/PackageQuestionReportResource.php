08a9efdf73aff933b6ec830a415e7bb56a3c6b03
saber13812002 <saber.tabatabaee@gmail.com>
2021-07-03 12:45:47 +0430

    fix: return all fields in question model

--- app/Http/Resources/PackageQuestionReportResource.php
+++ app/Http/Resources/PackageQuestionReportResource.php
@@ -24,10 +24,23 @@ class PackageQuestionReportResource extends BasicResource
         return [
             'id' => $resource->id,
             'title' => $resource->title,
-            "package_id" => $resource->package_id,
             "description" => $resource->description,
+
+            "answer_type_id" => $resource->answer_type_id,
+            "correct_choice_id" => $resource->correct_choice_id,
+
+            "order" => $resource->order,
+            "weight" => $resource->weight,
+            "is_active" => $resource->is_active,
+
+            "source_id" => $resource->source_id,
+            "event_ids" => $resource->event_ids,
+
+            "package_id" => $resource->package_id,
+
             'created_at' => $resource->created_at,
             'updated_at' => $resource->updated_at,
+
             'choices_results' => $choicesStatistics,
             'choices' => $choices,
         ];
