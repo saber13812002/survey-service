557367e4d20c4e8dfa145b374b69fd4bfcdc388b
saber13812002 <saber.tabatabaee@gmail.com>
2021-09-26 11:54:04 +0330

    fix: add line into devops file for apply changes in all env.

--- devops/2021-10-01-542384-persian-faker.php
+++ devops/2021-10-01-542384-persian-faker.php
@@ -3,11 +3,16 @@
 /*
 ENV: Production
 Services: survey-service
+Changes:
+ENV file:
+```PROXY_TOKENS=```
 Commands:
-
+```
 git pull
 composer i
 php artisan migrate
 php artisan migrate:fresh --seed
+```
+
 @hebrahimzadeh
 */
